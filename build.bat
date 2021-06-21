2231f74e9ce3fb96feeeb167d4e4b543f5d1c4fb
hkmadao <madaodesuka@hotmail.com>
2021-06-21 16:44:20 +0800

    打包脚本修改

--- build.bat
+++ build.bat
@@ -1,20 +1,48 @@
-@REM Windows
-SET GOOS=windows
-SET GOARCH=amd64
+@echo off
+chcp 65001
+set /p ver=请输入版本：  
+echo 版本：%ver% 打包开始
+@REM window_amd64
+@REM SET GOOS=windows
+@REM SET GOARCH=amd64
 SET CGO_ENABLED=1
-go build -o rtmp2flv.exe main.go
+go build -o rtmp2flv_%ver%_window_amd64.exe main.go
 
-rmdir /S /Q .\output\rtmp2flv
+@REM window_amd64
+@REM rmdir /S /Q .\output\rtmp2flv_%ver%_window_amd64
 
-md .\output\rtmp2flv\output\live
-md .\output\rtmp2flv\output\log
-md .\output\rtmp2flv\conf
+@REM md .\output\rtmp2flv_%ver%_window_amd64\output\live
+@REM md .\output\rtmp2flv_%ver%_window_amd64\output\log
+@REM md .\output\rtmp2flv_%ver%_window_amd64\conf
 
-xcopy /S /Y /E .\static .\output\rtmp2flv\static\
-xcopy /S /Y /E .\db .\output\rtmp2flv\db\
-xcopy .\conf\conf.yml .\output\rtmp2flv\conf
-xcopy .\rtmp2flv .\output\rtmp2flv
-xcopy .\rtmp2flv.exe .\output\rtmp2flv
-xcopy .\start.vbs .\output\rtmp2flv
+@REM xcopy /S /Y /E .\static .\output\rtmp2flv_%ver%_window_amd64\static\
+@REM xcopy /S /Y /E .\db .\output\rtmp2flv_%ver%_window_amd64\db\
+@REM xcopy .\conf\conf.yml .\output\rtmp2flv_%ver%_window_amd64\conf
+@REM xcopy .\rtmp2flv_%ver%_window_amd64.exe .\output\rtmp2flv_%ver%_window_amd64\rtmp2flv.exe
+@REM xcopy .\start.vbs .\output\rtmp2flv_%ver%_window_amd64\start.vbs
+
+@REM linux_amd64
+@REM rmdir /S /Q .\output\rtmp2flv_%ver%_linux_amd64
+
+@REM md .\output\rtmp2flv_%ver%_linux_amd64\output\live
+@REM md .\output\rtmp2flv_%ver%_linux_amd64\output\log
+@REM md .\output\rtmp2flv_%ver%_linux_amd64\conf
+
+@REM xcopy /S /Y /E .\static .\output\rtmp2flv_%ver%_linux_amd64\static\
+@REM xcopy /S /Y /E .\db .\output\rtmp2flv_%ver%_linux_amd64\db\
+@REM xcopy .\conf\conf.yml .\output\rtmp2flv_%ver%_linux_amd64\conf
+@REM xcopy .\rtmp2flv_%ver%_linux_amd64 .\output\rtmp2flv_%ver%_linux_amd64\rtmp2flv
+
+@REM linux_armv6
+@REM rmdir /S /Q .\output\rtmp2flv_%ver%_linux_armv6
+
+@REM md .\output\rtmp2flv_%ver%_linux_armv6\output\live
+@REM md .\output\rtmp2flv_%ver%_linux_armv6\output\log
+@REM md .\output\rtmp2flv_%ver%_linux_armv6\conf
+
+@REM xcopy /S /Y /E .\static .\output\rtmp2flv_%ver%_linux_armv6\static\
+@REM xcopy /S /Y /E .\db .\output\rtmp2flv_%ver%_linux_armv6\db\
+@REM xcopy .\conf\conf.yml .\output\rtmp2flv_%ver%_linux_armv6\conf
+@REM xcopy .\rtmp2flv_%ver%_linux_armv6 .\output\rtmp2flv_%ver%_linux_armv6\rtmp2flv
 
 pause
